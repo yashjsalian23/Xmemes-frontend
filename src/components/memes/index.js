40d61203e84aabb00efbc36531ca00ffbc4682e5
yashjsalian23 <yashjsalian23@gmail.com>
2021-02-08 18:42:34 +0530

    implemented snapshot for edit

--- src/components/memes/index.js
+++ src/components/memes/index.js
@@ -9,19 +9,29 @@ const index = () => {
     let [ memeArray, setMemeArray ] = useState([]);
 
     useEffect(async () => {
+        fetchMemes();
+    }, []);
+
+    let fetchMemes = async () => {
         const response = await fetch('http://localhost:8081/memes',{
             method: 'get'
         });
 
         const res = await response.json();
         setMemeArray(res);
-    }, []);
+    }
 
     return (
         <div className="memeList">
             <div>
                 {
-                    memeArray.map(meme => <MemeComponent key={meme.url} name={meme.name} url={meme.url} caption={meme.caption} />)
+                    memeArray.map(meme => <MemeComponent
+                        key={meme.id} 
+                        id={meme.id} 
+                        name={meme.name} 
+                        url={meme.url} 
+                        caption={meme.caption}
+                        fetchMemesFunc={fetchMemes} />)
                 }
             </div>
         </div>
--- src/components/memes/memeComponent/MemeComponent.js
+++ src/components/memes/memeComponent/MemeComponent.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './MemeComponent.css';
 
-const MemeComponent = ({name, url, caption}) => {
+const MemeComponent = ({name, url, caption, id, fetchMemesFunc}) => {
+
+    let [ editedUrl, setEditedUrl ] = useState(url);
+    let [ editedName, setEditedName ] = useState(name);
+    let [ editedCaption, setEditedCaption ] = useState(caption);
+    let [ showForm, setShowForm ] = useState(true);
+
+    let editMemeHandler = async (event) => {
+        event.preventDefault();
+        let body ={
+            url:editedUrl,
+            caption:editedCaption,
+            name:editedName
+        };
+        const response = await fetch(`http://localhost:8081/memes/${id}`,{
+            method:'PATCH',
+            body: JSON.stringify(body),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const res = await response.json();
+        if(res.success){
+            fetchMemesFunc();
+        }
+    }
+
+    let editForm = showForm? <React.Fragment>
+        <form onSubmit={editMemeHandler}>
+            <input type="text" value={editedCaption} onChange={(e) => setEditedCaption(e.target.value)} />
+            <button type="submit">Submit</button>
+        </form>
+    </React.Fragment>:null
+
     return (
         <div className="meme">
             <p className="meme-name">{name}</p>
             <img src={url} alt={caption} />
+            <div>
+                <button onClick={() => setShowForm(true)}>Edit</button>
+            </div>
             <p className="meme-caption">{caption}</p>
             <hr style={{fontWeight:500}} />
+            { editForm}
         </div>
     );
 };
